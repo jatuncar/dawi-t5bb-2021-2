2fa0ab3debcdf26e91af82dbe1b33b92f5da010b
jatuncar <jatuncar@correo.com>
2021-09-17 20:31:19 -0500

    Terminado el listado de productos (Tarea)

--- src/main/java/app/FrmManteProd.java
+++ src/main/java/app/FrmManteProd.java
@@ -13,6 +13,7 @@ import model.Usuario;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.util.List;
@@ -26,13 +27,14 @@ import javax.swing.JComboBox;
 public class FrmManteProd extends JFrame {
 
 	private JPanel contentPane;
-	
+
 	private JTextArea txtSalida;
 	private JTextField txtCódigo;
 	JComboBox cboCategorias;
 	private JTextField txtDescripcion;
 	private JTextField txtStock;
 	private JTextField txtPrecio;
+
 	/**
 	 * Launch the application.
 	 */
@@ -69,14 +71,14 @@ public class FrmManteProd extends JFrame {
 		});
 		btnNewButton.setBounds(324, 29, 89, 23);
 		contentPane.add(btnNewButton);
-		
+
 		JScrollPane scrollPane = new JScrollPane();
 		scrollPane.setBounds(10, 171, 414, 143);
 		contentPane.add(scrollPane);
-		
+
 		txtSalida = new JTextArea();
 		scrollPane.setViewportView(txtSalida);
-		
+
 		JButton btnListado = new JButton("Listado");
 		btnListado.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -85,64 +87,82 @@ public class FrmManteProd extends JFrame {
 		});
 		btnListado.setBounds(177, 322, 89, 23);
 		contentPane.add(btnListado);
-		
+
 		txtCódigo = new JTextField();
 		txtCódigo.setBounds(122, 11, 86, 20);
 		contentPane.add(txtCódigo);
 		txtCódigo.setColumns(10);
-		
+
 		JLabel lblCodigo = new JLabel("Id. Producto :");
 		lblCodigo.setBounds(10, 14, 102, 14);
 		contentPane.add(lblCodigo);
-		
+
 		cboCategorias = new JComboBox();
 		cboCategorias.setBounds(122, 70, 86, 22);
 		contentPane.add(cboCategorias);
-		
+
 		JLabel lblCategora = new JLabel("Categor\u00EDa");
 		lblCategora.setBounds(10, 74, 102, 14);
 		contentPane.add(lblCategora);
-		
+
 		JLabel lblNomProducto = new JLabel("Nom. Producto :");
 		lblNomProducto.setBounds(10, 45, 102, 14);
 		contentPane.add(lblNomProducto);
-		
+
 		txtDescripcion = new JTextField();
 		txtDescripcion.setColumns(10);
 		txtDescripcion.setBounds(122, 42, 144, 20);
 		contentPane.add(txtDescripcion);
-		
+
 		JLabel lblStock = new JLabel("Stock:");
 		lblStock.setBounds(10, 106, 102, 14);
 		contentPane.add(lblStock);
-		
+
 		txtStock = new JTextField();
 		txtStock.setColumns(10);
 		txtStock.setBounds(122, 103, 77, 20);
 		contentPane.add(txtStock);
-		
+
 		JLabel lblPrecio = new JLabel("Precio:");
 		lblPrecio.setBounds(10, 134, 102, 14);
 		contentPane.add(lblPrecio);
-		
+
 		txtPrecio = new JTextField();
 		txtPrecio.setColumns(10);
 		txtPrecio.setBounds(122, 131, 77, 20);
 		contentPane.add(txtPrecio);
-		
+
 		llenaCombo();
 	}
 
 	void llenaCombo() {
-		
+
 	}
-	
+
 	void listado() {
-		
+		EntityManagerFactory fabrica = Persistence.createEntityManagerFactory("mysql");
+
+		EntityManager em = fabrica.createEntityManager();
+
+		// listado de los usuarios
+		// select * from tb_usuario
+		TypedQuery<Usuario> query = em.createQuery("Select u from Usuario u", Usuario.class);
+		List<Usuario> lstUsuarios = query.getResultList();
+
+		System.out.println("Cantidad de usuarios : " + lstUsuarios.size());
+		if (lstUsuarios.size() == 0) {
+			System.out.println("Listado vacío");
+		} else {
+			System.out.println("Listado de usuarios");
+			for (Usuario u : lstUsuarios) {
+				System.out.println(">>> " + u);
+			}
+		}
+
+		em.close();
 	}
-	
+
 	void registrar() {
-		
 
 	}
 }
